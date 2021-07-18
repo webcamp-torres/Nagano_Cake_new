875ba622a5555f34e65168523e9ead92d9d045a0
ogmegumi <ogmegumi@gmail.com>
2021-07-18 02:15:31 +0000

    add:search

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,21 @@
 class Genre < ApplicationRecord
+    has_many :items, dependent: :destroy
+    validates :name, presence: true
+    
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    attribute :body, :string
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Genre.where(title: content)
+    elsif method == 'forward'
+      Genre.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Genre.where('title LIKE ?', '%'+content)
+    else
+      Genre.where('title LIKE ?', '%'+content+'%')
+    end
+  end
 end
--- test/controllers/admin/search_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admin::SearchControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
