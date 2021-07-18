99e20677a42f428da7e803704972ff518033f6a0
makisan3 <nerimaki921@gmail.com>
2021-07-18 12:08:45 +0000

    Fix admin/items/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,7 +10,7 @@ class Admin::ItemsController < ApplicationController
 
    def create
       @item = Item.new(item_params)
-      @item.save!
+      @item.save
       redirect_to admin_items_path
    end
 
