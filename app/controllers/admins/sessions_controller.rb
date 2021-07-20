1701df2ec7c93f41d5fdddd1317f257d044c4068
EC2 Default User <ec2-user@ip-172-31-9-152.ec2.internal>
2021-07-20 03:59:44 +0000

    [Add]

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -4,22 +4,28 @@ class Admins::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
+  # def new
   #   super
-  end
+  # end
 
   # POST /resource/sign_in
-  def create
+  # def create
   #   super
-  end
+  # end
 
   # DELETE /resource/sign_out
-  def destroy
+  # def destroy
   #   super
+  # end
+
+  protected
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
-  # protected
-
+  def after_sign_in_path_for(resource)
+    root_path
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
