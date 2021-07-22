fccd98baa0fce281bdfa5018d3377748181cad43
ogmegumi <ogmegumi@gmail.com>
2021-07-22 09:24:07 +0000

    再度追記

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
 before_action :authenticate_admin!
 
   def show
-    @orders = Order.find(params[:id])
-    @orders
+    @order = Order.find(params[:id])
   end
+
 end
