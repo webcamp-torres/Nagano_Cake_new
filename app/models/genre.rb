d4f618831bdf9975f95f96cbbb66b3caf76a3eb7
ogmegumi <ogmegumi@gmail.com>
2021-07-22 02:11:28 +0000

    fix:cart_item/デザイン変更

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,12 +1,6 @@
 class Genre < ApplicationRecord
     has_many :items, dependent: :destroy
 
-    enum genre:{
-     ケーキ: 0,
-     プリン: 1,
-     焼き菓子: 2,
-     キャンディ: 3
-    }
     validates :name, presence: true
 
     # include ActiveModel::Model
@@ -14,15 +8,4 @@ class Genre < ApplicationRecord
 
     # attribute :body, :string
 
-  def self.search_for(content, method)
-    if method == 'perfect'
-      Genre.where(title: content)
-    elsif method == 'forward'
-      Genre.where('title LIKE ?', content+'%')
-    elsif method == 'backward'
-      Genre.where('title LIKE ?', '%'+content)
-    else
-      Genre.where('title LIKE ?', '%'+content+'%')
-    end
-  end
 end
