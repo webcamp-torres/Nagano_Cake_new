68b027b31dba9f41c6f93ebd102eb6faf8aa4ac3
EC2 Default User <ec2-user@ip-172-31-4-72.ec2.internal>
2021-07-22 02:10:43 +0000

    mypage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_023205) do
+ActiveRecord::Schema.define(version: 2021_07_21_040548) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_023205) do
     t.string "name", null: false
     t.string "image_id", null: false
     t.text "introduction", null: false
-    t.integer "price", null: false
+    t.integer "price", default: 0, null: false
     t.boolean "sales_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_023205) do
     t.boolean "unsubscribe_status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_valid", default: true, null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
