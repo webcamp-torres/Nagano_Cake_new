f1b521aaa71c95aa8566f75ec7a8ad5e3c82230c
EC2 Default User <ec2-user@ip-172-31-9-152.ec2.internal>
2021-07-23 02:54:46 +0000

    [Add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,5 @@
 class Admin::OrdersController < ApplicationController
-# before_action :authenticate_admin!
-
+  
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items
--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,5 +1,5 @@
 class Public::MembersController < ApplicationController
-  
+  before_action :authenticate_member!
   def my_page
     @member = current_member 
   end
