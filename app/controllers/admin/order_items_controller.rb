b404ff9c0ea9fd0392124d518e9f41f99cf08a43
ogmegumi <ogmegumi@gmail.com>
2021-07-24 13:04:40 +0000

    fix:admin/order_items add:販売ステータス/注文ステータス/製作スタータス

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,15 +3,25 @@ class Admin::OrderItemsController < ApplicationController
   def update
     order_item = OrderItem.find(params[:order_item][:order_id])
 	  order_item.update(making_status: params[:order_item][:making_status].to_i)
+	  # binding.pry
+	  if params[:order_item][:making_status].to_i == 2 #paramsで受け取った数
+	  	order_item.order.update(order_status: 2)
+	  elsif order_item.order.order_items.count == order_item.order.order_items.where(making_status: 3).count
+	  	order_item.order.update(order_status: 3)
+	  end
+    redirect_to admin_order_path(order_item.order.id)
 
-		case order_item.making_status
-		 when "製作中"
-				order_item.order.update(order_status: "製作中")
-		 when "製作完了"
-		   if order_item.order.order_items.all?{|order_item| order_item.making_status == "製作完了"} #each文から取り出した全部が製作完了になったら
-				order_item.order.update(order_status: "発送準備中")
-		   end
-			redirect_to admin_order_path(order_item.order.id)
-     end
-    end
+
+		# # case order_item.making_status
+		# #when "製作中"
+		# 	  	# order_item.order.update(order_status: "製作中")
+		# 	    redirect_to admin_order_path(order_item.order.id)
+		# end
+		# when "製作完了"
+		#   if order_item.order.order_items.all?{|order_item| order_item.making_status == "製作完了"} #ブロック変数から取り出した全部が製作完了になったら
+		# 	  	order_item.order.update(order_status: "発送準備中")
+		#   end
+		    	# redirect_to admin_order_path(order_item.order.id)
+  #   end
+  end
 end
